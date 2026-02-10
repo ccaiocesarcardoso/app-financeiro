6a6d899caa648cabb68bc2b8358077731c52ffb0
👉 Caio César Cardoso <ccaiocesar648@gmail.com>
2026-02-10 11:08:08 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'finance-control-v2';
+const CACHE_NAME = 'finance-control-v4';
 const ASSETS = [
     './',
     './index.html',
     './style.css',
     './script.js',
     './manifest.json',
-    './icon-512.png',
+    './icone.png',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
     'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
 ];
